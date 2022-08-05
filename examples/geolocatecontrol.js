baee5ca9a1083b15536af18ea4b01c0c5a006808
Sven Möller <sven.moeller@mapz.com>
2022-08-05 09:37:16 +0200

    Fix attribution in geolocatecontrol example

--- examples/geolocatecontrol.js
+++ examples/geolocatecontrol.js
@@ -3,17 +3,14 @@ import Map from '../src/ol/Map.js';
 import Tile from '../src/ol/layer/Tile.js';
 import View from '../src/ol/View.js';
 import XYZ from '../src/ol/source/XYZ.js';
-import {Attribution} from '../src/ol/control.js';
 import {transform} from '../src/ol/proj.js';
 
 const baseLayer = new Tile({
   source: new XYZ({
     attributions: [
-      new Attribution({
-        html: '© 2022 <a target="_blank" href="http://www.mapz.com">mapz.com </a>\
-            - Map Data: <a target="_blank" href="http://openstreetmap.org" >OpenStreetMap</a>\
-            (<a href="http://opendatacommons.org/licenses/odbl/1.0/" target="_blank">ODbL</a>)',
-      }),
+      '© 2022 <a target="_blank" href="http://www.mapz.com">mapz.com </a>\
+                      - Map Data: <a target="_blank" href="http://openstreetmap.org" >OpenStreetMap</a>\
+                      (<a href="http://opendatacommons.org/licenses/odbl/1.0/" target="_blank">ODbL</a>)',
     ],
     tilePixelRatio: 2,
     url: 'https://tiles.mapz.com/mapproxy/v1/demo-817ca352/tiles/1.0.0/mapz_multicolor_base_hq/EPSG3857/{z}/{x}/{-y}.jpeg',
